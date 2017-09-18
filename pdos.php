dc443c8edffa59179fd1a8faa8b83a9e840f9a7f
Nicolas Onofrio <nico.onofrio@gmail.com>
2017-09-18 21:11:54 +0800

    Update pdos.php

--- pdos.php
+++ pdos.php
@@ -1,1 +1,32 @@
 
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>&#9776; Nicolas Onofrio</title>
+	<meta charset="utf-8" />
+        <meta name="viewport" content="user-scalable = yes">
+	<link rel="stylesheet" href="css/pdos.css" />
+    </head>
+    <body>
+
+    <!-- Header -->
+
+    <!-- Content -->
+    <div id="content-wrapper">
+        <div id="content">
+            <div class="container">
+            
+                <h1>Projected DOS of doped TMDs</h1></div>
+
+                <p>Select the doping type.</p>
+                <ul style="list-style-position: inside">
+                    <li><a href="pdos-substitutional.php">Substitutional doping</a></li> 
+                    <li><a href="pdos-interstitial.php">Interstitial doping</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+
+    </body>
+</html>
