517dd5aea68985baa7acdaedae270406fbb7b2ae
Nicolas Onofrio <nico.onofrio@gmail.com>
2017-10-02 17:47:08 +0800

    Add files via upload

---
+++ nonofrio.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Nicolas Onofrio</title>
+	<meta charset="utf-8" />
+        <meta name="viewport" content="user-scalable = yes">
+	<link rel="stylesheet" href="css/pdos.css" />
+    </head>
+    <body>
+
+    <!-- Content -->
+    <div id="content-wrapper">
+        <div id="content">
+            <div class="container">
+            
+                <h1>Online supplements</h1></div>
+
+                <ul style="list-style-position: inside">
+                    <li><a href="pdos-substitutional.php">Substitutional doping in TMDs</a></li> 
+                    <li><a href="pdos-interstitial.php">Interstitial doping in TMDs</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+
+    </body>
+</html>
